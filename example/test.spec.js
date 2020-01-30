7a607933ae42152b0c7eaea46e5d39a4be591225
ElenaRomanchuk <elena_romanchuk@epam.com>
2020-01-30 09:36:37 +0300

    EPMRPP-45147. ESLint.

---
+++ example/test.spec.js
@@ -0,0 +1,58 @@
+const expect = require('chai').expect;
+
+describe('describe', function() {
+  let foo = '';
+  before(function() {
+    foo = 'bar';
+  });
+
+  beforeEach(function() {
+    foo = 'bar';
+  });
+
+  describe('nested describe', function() {
+    before(function() {
+      foo = 'bar';
+    });
+
+    beforeEach(function() {
+      foo = 'bar';
+    });
+
+    it('test pass in nested describe', function() {
+      expect(foo).to.be.equal('bar');
+    });
+
+    it('test fail in nested describe', function() {
+      expect(true).to.be.equal(false);
+    });
+
+    it('test pending in nested describe');
+
+    after('named hook', function() {
+      foo = 'bar';
+    });
+
+    afterEach(function() {
+      foo = 'bar';
+    });
+  });
+
+  it('test pass', function() {
+    expect(true).to.be.equal(true);
+  });
+
+  it('test fail', function() {
+    expect(true).to.be.equal(false);
+  });
+
+  it('test pending');
+
+  after(function() {
+    foo = 'bar';
+  });
+
+  afterEach(function() {
+    foo = 'bar';
+  });
+});
