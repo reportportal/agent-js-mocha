a185bbad33c0b13aa7cf892b3923e6e89a86b41b
ElenaRomanchuk <elena_romanchuk@epam.com>
2020-03-31 19:33:58 +0300

    EPMRPP-49184. Code reference for tests and suites.

---
+++ test/utils.test.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { getCodeRef } = require('./../lib/utils');
+
+describe('utils', function() {
+  it('should return correct code ref', function() {
+    const mockedTest = {
+      title: 'testTitle',
+      file: `${process.cwd()}${path.sep}test${path.sep}example.js`,
+      titlePath: () => ['rootDescribe', 'parentDescribe', 'testTitle'],
+    };
+    const expectedCodeRef = `test/example.js:rootDescribe.parentDescribe.testTitle`;
+
+    const codeRef = getCodeRef(mockedTest);
+
+    expect(codeRef).toEqual(expectedCodeRef);
+  });
+});
