a0800c51a68f75be3cc36f67ed57a3a1fbedb71e
ElenaRomanchuk <elena_romanchuk@epam.com>
2020-04-15 11:43:22 +0300

    EPMRPP-49770. Implement a collecting of Google analytics event: Name and version of Agent

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,22 +1,32 @@
-const { getCodeRef } = require('./../lib/utils');
+const { getCodeRef, getAgentInfo } = require('./../lib/utils');
 
 describe('utils', function() {
-  it('should return correct code ref', function() {
-    jest.mock('path', () => ({
-      sep: '\\',
-    }));
-    jest.spyOn(process, 'cwd').mockImplementation(() => 'C:\\testProject');
-    const mockedTest = {
-      title: 'testTitle',
-      file: `C:\\testProject\\test\\example.js`,
-      titlePath: () => ['rootDescribe', 'parentDescribe', 'testTitle'],
-    };
-    const expectedCodeRef = `test/example.js/rootDescribe/parentDescribe/testTitle`;
+  describe('getCodeRef', function() {
+    it('should return correct code ref', function() {
+      jest.mock('path', () => ({
+        sep: '\\',
+      }));
+      jest.spyOn(process, 'cwd').mockImplementation(() => 'C:\\testProject');
+      const mockedTest = {
+        title: 'testTitle',
+        file: `C:\\testProject\\test\\example.js`,
+        titlePath: () => ['rootDescribe', 'parentDescribe', 'testTitle'],
+      };
+      const expectedCodeRef = `test/example.js/rootDescribe/parentDescribe/testTitle`;
 
-    const codeRef = getCodeRef(mockedTest);
+      const codeRef = getCodeRef(mockedTest);
 
-    expect(codeRef).toEqual(expectedCodeRef);
+      expect(codeRef).toEqual(expectedCodeRef);
 
-    jest.clearAllMocks();
+      jest.clearAllMocks();
+    });
+  });
+  describe('getAgentInfo', function() {
+    it('should contain version and name properties', function() {
+      const agentInfo = getAgentInfo();
+
+      expect(Object.keys(agentInfo)).toContain('version');
+      expect(Object.keys(agentInfo)).toContain('name');
+    });
   });
 });
