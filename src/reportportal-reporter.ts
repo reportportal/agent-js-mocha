780f7841657dbefd95bd18630c599ba1e6394452
Anatoly Kuznetsov <aikuznetssov@gmail.com>
2022-07-05 11:06:48 -0700

    fix import Mocha

--- src/reportportal-reporter.ts
+++ src/reportportal-reporter.ts
@@ -1,4 +1,4 @@
-const Mocha = require('Mocha')
+import Mocha from 'mocha'
 import { ReportportalParallelRunAgent } from './reportportal-parallel-run-agent'
 const ReportportalAgent = require('@reportportal/agent-js-mocha/lib/mochaReporter')
 
@@ -7,7 +7,10 @@ export class ReportportalReporter extends Mocha.reporters.Base {
     super(runner, options)
     if (options.parallel === true) {
       process.env.RETURNLY_REPORTPORTAL_PARALLEL = 'true'
-      return new ReportportalParallelRunAgent(runner, options)
+      return new ReportportalParallelRunAgent(
+        runner,
+        options
+      ) as unknown as Mocha.reporters.Base
     } else {
       process.env.RETURNLY_REPORTPORTAL_PARALLEL = 'false'
       return new ReportportalAgent(runner, options)
